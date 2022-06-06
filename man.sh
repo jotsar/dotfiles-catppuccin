76aad7393c0e81a0fff9dd8e7f6d786cae9ea7a6
js <jssjdpl@gmail.com>
2022-06-06 22:04:45 +0200

    more tweaks

--- man.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ~/dotfiles-catppuccin
-rm -rf ~/.config/i3 ~/.config/btop ~/.config/gtk-3.0 ~/.config/kitty ~/.config/nitrogen ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/neofetch
-mv config/* ~/.config/
-mkdir -p ~/Pictures/wallpaper/
-mv wallpaper/* ~/Pictures/wallpaper
-mkdir -p ~/.local/bin
-mv bin/* ~/.local/bin/
-
